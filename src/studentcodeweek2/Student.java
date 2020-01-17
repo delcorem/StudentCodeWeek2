358e87bfdc4019b7a128cb0c2bd6778127acdec4
delcorem <delcorem@sheridancollege.ca>
2020-01-17 13:03:14 -0500

    Changed date

--- src/studentcodeweek2/Student.java
+++ src/studentcodeweek2/Student.java
@@ -8,7 +8,7 @@ package studentcodeweek2;
 /**
  * A class to model students. Students can 
  * study. They have a name and an ID.
- * @author dancye, 2020
+ * @author dancye, 20202020
  */
 public class Student 
 {
